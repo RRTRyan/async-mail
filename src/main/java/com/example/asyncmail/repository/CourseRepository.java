d4537a57bcdaa4e3eea052d3cf25b396acf99b84
RRTRyan <tiaryanrzk@gmail.com>
2026-07-06 09:51:01 +0300

    feat: add repository classes

---
+++ src/main/java/com/example/asyncmail/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.example.asyncmail.repository;
+
+import com.example.asyncmail.repository.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, UUID> {
+}
