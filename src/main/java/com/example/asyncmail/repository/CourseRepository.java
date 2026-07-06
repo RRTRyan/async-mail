8ff4e40982c24586bba6833aed81b04492b43cf3
RRTRyan <tiaryanrzk@gmail.com>
2026-07-06 16:32:17 +0300

    feat: add registration handling classes

--- src/main/java/com/example/asyncmail/repository/CourseRepository.java
+++ src/main/java/com/example/asyncmail/repository/CourseRepository.java
@@ -1,11 +1,9 @@
 package com.example.asyncmail.repository;
 
 import com.example.asyncmail.repository.model.Course;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface CourseRepository extends JpaRepository<Course, UUID> {
-}
+public interface CourseRepository extends JpaRepository<Course, UUID> {}
---
+++ src/main/java/com/example/asyncmail/service/event/RegistrationEmailService.java
@@ -0,0 +1,36 @@
+package com.example.asyncmail.service.event;
+
+import com.example.asyncmail.endpoint.event.model.RegistrationEmail;
+import com.example.asyncmail.mail.Email;
+import com.example.asyncmail.mail.Mailer;
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
+import java.util.function.Consumer;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RegistrationEmailService implements Consumer<RegistrationEmail> {
+  private final Mailer mailer;
+
+  @Override
+  @SneakyThrows
+  public void accept(RegistrationEmail registrationEmail) {
+    InternetAddress emailAddress = new InternetAddress(registrationEmail.getTo());
+    mailer.accept(
+        new Email(
+            new InternetAddress(emailAddress.getAddress()),
+            List.of(),
+            List.of(),
+            "Course registration",
+            "Your registration to the course have been successfully saved",
+            List.of()));
+  }
+
+  @Override
+  public Consumer<RegistrationEmail> andThen(Consumer<? super RegistrationEmail> after) {
+    return Consumer.super.andThen(after);
+  }
+}
